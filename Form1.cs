3a31115bc8ac179f4dee5ef855e698be27794ab4
Stagiaire <Stagiaire@DESKTOP-B8734V5>
2024-01-15 16:53:32 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+namespace ApplicationSNMP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = txt
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
